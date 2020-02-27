97136285f61eb0e52ef28e3c50f53e050b335bcf
mbru78 <mbru@sodifrance.fr>
2020-02-27 16:07:30 +0100

    map integration

--- src/app/input-adress/input-adress.component.ts
+++ src/app/input-adress/input-adress.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, forwardRef } from '@angular/core';
 import places from 'places.js';
 import {NG_VALUE_ACCESSOR, ControlValueAccessor} from '@angular/forms';
+import * as leaflet from 'leaflet';
 
 @Component({
   selector: 'app-input-adress',
@@ -20,6 +21,7 @@ export class InputAdressComponent implements OnInit, AfterViewInit, ControlValue
   private onChange;
   private onTouched;
   private autocomplete;
+  public map;
 
   public innerValue = {
     address: '',
@@ -28,7 +30,7 @@ export class InputAdressComponent implements OnInit, AfterViewInit, ControlValue
   };
 
   @ViewChild('ref', { static: true }) public el: ElementRef;
-
+  @ViewChild('map', { static: true }) public mapEl: ElementRef;
 
   constructor() { }
   writeValue(value){
@@ -44,7 +46,7 @@ export class InputAdressComponent implements OnInit, AfterViewInit, ControlValue
   ngOnInit() {
   }
   setDisabledState(disabled: boolean){}
-  
+
   ngAfterViewInit() {
     this.autocomplete = places({
       appId: 'pl4R2GKC8AAF',
@@ -58,13 +60,38 @@ export class InputAdressComponent implements OnInit, AfterViewInit, ControlValue
         this.innerValue.lng = e.suggestion.latlng.lng;
         this.innerValue.address = e.suggestion.value;
         this.onChange(this.innerValue);
+        this.addMarker();
       }
     });
+
+
+    this.map = leaflet.map(this.mapEl.nativeElement);
+    if (this.innerValue.lat) {
+      this.addMarker();
+    } else {
+      this.map.setView([48.864716, 2.349014], 13);
+    }
+    leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors'
+    }).addTo(this.map);
+
   }
+  private addMarker() {
+    const coord: [number, number] = [ this.innerValue.lat, this.innerValue.lng ];
+    leaflet.marker(coord).addTo(this.map);
+    this.map.setView(coord, 13);
+  }
+ 
 
 }
 
 
+
+
+
+
+
+    
 
 
 
